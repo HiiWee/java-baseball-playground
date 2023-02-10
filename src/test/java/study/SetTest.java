33db6943531b76ae678fa2aded21bf52cd092b80
HiiWee <wpdlzhf159@gmail.com>
2023-02-10 17:42:32 +0900

    test: Set 학습 테스트 요구사항2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,13 +1,15 @@
 package study;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -31,5 +33,11 @@ public class SetTest {
         assertThat(size).isEqualTo(3);
     }
 
-
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 값 존재 테스트")
+    void setContains(int value) {
+        // then
+        assertThat(numbers.contains(value)).isTrue();
+    }
 }
